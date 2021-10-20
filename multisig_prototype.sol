575f564fbadf96ed6c768c6d4fbc2b5127cbbbb9
Cheah KK <ckarkeong@gmail.com>
2021-10-21 00:44:36 +0800

    Feature updates
    Functions added are assigning sub-owners to a particular deposit, updating and checking of transfer setting to allow which addresses to request for transfer, halfway through the function of requesting transfer
    
    Commit type: feat, refactor
    
    [Ticket: #2]

--- multisig_prototype.sol
+++ multisig_prototype.sol
@@ -18,18 +18,28 @@ contract Deposit {
     
     
     constructor(){
-     
+     address owner = msg.sender;
     }
     
     //double mapping
-    mapping(address => mapping(uint => mapping(uint => bool)))depositID;
+    mapping(address => mapping(uint => uint))depositID;
     mapping(address => mapping(uint => bool))approvals;
     mapping(address => mapping(uint => uint))transactionID; //possible to triple map?
+    mapping(address => mapping(uint => uint))transferSetupID;
     mapping(address => mapping(uint => uint))balance;
+    mapping(address => mapping(uint => address))depositOwner;
     //mapping(address => uint) transactionID;
    
     address[] walletOwners;
+    address payable[] clearToAdd;
+    address payable[] adr;
+    address payable[] toAdd;
     
+    struct testPush{
+         address payable[] depositUser;
+         address payable[] approvedUsersForTransferRequest;
+    } 
+ 
     struct depositRecord {
        uint txnId;
        address depositUser;
@@ -42,11 +52,14 @@ contract Deposit {
    
     struct transferSetting {
        uint txnId;
-       address depositUser;
-       address approvedUsersForTransferRequest;
+       address payable[] depositUser;
+       bool withdrawStatus;
+       
    }
    
    transferSetting[] transferSetup;
+   
+   
    
    struct transferRequest {
        uint txnId;
@@ -74,60 +87,68 @@ contract Deposit {
        uint mappedTxnId = _txnId;
        
        transactionID[msg.sender][_txnId] = mappedTxnId;
+       depositOwner[msg.sender][_txnId] = msg.sender; 
        balance[msg.sender][_txnId] = msg.value;
        
        for (uint i = 0; i < InitialDeposit.length; i++) {
            require(InitialDeposit[i].txnId !=  transactionID[msg.sender][_txnId], "txnId duplicate, please key in an unused random deposit transaction ID");
        }
         InitialDeposit.push(depositRecord(transactionID[msg.sender][_txnId], msg.sender, _totalOwners, _approvalsForTransfer, msg.value));
+        depositID[msg.sender][_txnId] = InitialDeposit.length;
         return "Deposit success, please key in deposit transaction ID in the future to check deposit";
     }
     
-    function findDeposit(uint _txnId) public view returns (depositRecord memory display) {
-        for (uint i = 0; i < InitialDeposit.length; i++) {
-                if (InitialDeposit[i].txnId == _txnId) {
-                    return InitialDeposit[i];
-                } 
-                    
-                }
-
+    //For deposit owner to track their deposit
+    function findDeposit(uint _txnId) public view returns (depositRecord memory) {
+        uint depositNumber = depositID[msg.sender][_txnId];
+        return InitialDeposit[depositNumber - 1];
+    }
+        
+    
+    function assignSubOwners(uint _txnId, address payable _subOwner)public payable returns (string memory){
+        uint depositNumber  = depositID[msg.sender][_txnId];
+        if(InitialDeposit[depositNumber - 1].numberOfOwners > toAdd.length){
+        toAdd.push(_subOwner);
+        } 
+        
+        if (InitialDeposit[depositNumber - 1].numberOfOwners == toAdd.length) {
+            transferSetup.push(transferSetting(depositNumber, toAdd, false));
+            uint transferNumber = transferSetup.length;
+            transferSetupID[msg.sender][_txnId] = transferNumber;
+            toAdd = clearToAdd;
+            return "All subOwners Filled";
+        }
+        return "it works";
+    }
+        
+    function checkTransferSetting(uint _txnId)public view returns (transferSetting memory){
+        uint transferNumber = transferSetupID[msg.sender][_txnId];
+        return transferSetup[transferNumber - 1] ;
+    }
+    
+    function requestForTransfer(uint _txnId, address _mainOwner, uint valueOfTransfer, address _transferTo) public returns (string memory){
+        uint transferNumber = transferSetupID[_mainOwner][_txnId];
+        bool scanStatus = false;
+        for (uint i = 0; transferSetup[transferNumber - 1].depositUser.length >= i; i++){
+            if (transferSetup[transferNumber - 1].depositUser[i] ==  msg.sender){
+                scanStatus = true;
+                return "You may continue, verified Owner/SubOwner" ;
+                
             }
+        }
+        if (scanStatus == false){
+            return "You are not verified for the transaction stated, please key in txn ID again";
+        } 
+        }
+    
+    function recordTransferRequest(uint _txnId, address _mainOwner, uint valueOfTransfer, address _transferTo)private {
         
-    
-    
-    function assignTotalOwner()public{
-        
-    }
-    
-    function testFunction(uint _button, uint _txnId) public view returns(uint, uint) {
-        uint buttonTest = _button;
-        return (buttonTest, balance[msg.sender][_txnId]);
     }
     
     
     
-   
-    struct documentHolder {
-        uint Number;
-        string fileName;
-        bool approvalStatus;
-    }
-    
-    
-    
-    documentHolder[] myDocument;
-    
-    
-    function addDocument(uint _number, string memory _fileName) public {
-        myDocument.push(documentHolder(_number,_fileName, false));
-    }
-    
-    function retrieveDocument(uint _index) view public returns(documentHolder memory){
-        return myDocument[_index];
-    }
-    function checkApproval(uint _transferid) public view returns (bool){
-        bool approvalStatus = approvals[msg.sender][_transferid];
-        return approvalStatus;
-    }
     
 }
+        
+        
+        
